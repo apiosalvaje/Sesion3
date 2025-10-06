c9402f637d4eb2e79ad128f9cf9336f69ff9c6cb
pepel <loquedigamimujer12@gmail.com>
2025-10-06 18:28:41 +0200

    TDD 2º paso. Solventar pruebas

--- src/Test/testCalculadora.java
+++ src/Test/testCalculadora.java
@@ -8,21 +8,23 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import pkg.Calculadora;
+
 class testCalculadora {
 
 	@Test
 	void testSuma() {
 		assertEquals(5, Calculadora.suma(2,3));
 	}
-	
+	@Test
 	void testResta() {
 		assertEquals(1, Calculadora.resta(4,3));
 	}
-	
+	@Test
 	void testMultiplica() {
 		assertEquals(6, Calculadora.multiplica(2,3));
 	}
-	
+	@Test
 	void testDivide() {
 		assertEquals(2, Calculadora.divide(6,3));
 	}
